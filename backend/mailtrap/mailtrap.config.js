3161d3ec505dc9da07bf02a0fdbddce5a0e6f42a
Rahul2282 <rahul.mittal@skewb.ai>
2025-03-12 12:06:53 +0530

    added cors

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -4,9 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const mailtrapClient = new MailtrapClient({
-	// endpoint: process.env.MAILTRAP_ENDPOINT,
+	
 	token: process.env.MAILTRAP_TOKEN,
-	// testInboxId: process.env.MAILTRAP_ID,
 });
 
 export const  sender = {
